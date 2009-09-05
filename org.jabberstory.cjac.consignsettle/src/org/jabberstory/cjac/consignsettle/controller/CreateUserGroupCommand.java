de179212b7ff11b34eb72e837ee5f9d5b9b41b18
greatshin2 <greatshin2@18a45de0-8707-11de-82e5-a1837a67dc72>
2009-09-05 13:56:09 +0000

    사용자관리 원래대로 롤백

--- org.jabberstory.cjac.consignsettle/src/org/jabberstory/cjac/consignsettle/controller/CreateUserGroupCommand.java
+++ org.jabberstory.cjac.consignsettle/src/org/jabberstory/cjac/consignsettle/controller/CreateUserGroupCommand.java
@@ -1,8 +1,16 @@
 package org.jabberstory.cjac.consignsettle.controller;
 
 public class CreateUserGroupCommand {
+	private String groupId;
 	private String groupName;
 	private String role;
+	
+	public String getGroupId() {
+		return groupId;
+	}
+	public void setGroupId(String groupId) {
+		this.groupId = groupId;
+	}
 	
 	public void setGroupName(String groupName) {
 		this.groupName = groupName;
--- org.jabberstory.cjac.consignsettle/src/org/jabberstory/cjac/consignsettle/controller/UpdateUserGroupController.java
+++ org.jabberstory.cjac.consignsettle/src/org/jabberstory/cjac/consignsettle/controller/UpdateUserGroupController.java
@@ -5,8 +5,13 @@ import java.util.Map;
 import java.util.TreeMap;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
+import org.jabberstory.cjac.consignsettle.domain.UserGroup;
 import org.jabberstory.cjac.consignsettle.domain.UserService;
+import org.springframework.validation.BindException;
+import org.springframework.validation.ValidationUtils;
+import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.SimpleFormController;
 
 public class UpdateUserGroupController extends SimpleFormController {
@@ -35,32 +40,31 @@ public class UpdateUserGroupController extends SimpleFormController {
 		return map;
 	}
 	
-//	@Override
-//	protected Object formBackingObject(HttpServletRequest request)
-//			throws Exception {
-//		if (isFormSubmission(request)) {
-//			return super.formBackingObject(request);
-//		}
-//		CreateUserGroupCommand command = new CreateUserGroupCommand();
-//		String groupId = request.getParameter("groupId");
-//		UserGroup group = userService.getUserGroup(groupId);
-//		if (group != null) {
-//			command.setGroupId(groupId);
-//			command.setName(group.getName());
-//			command.setRole(group.getRole());
-//		}
-//		return command;
-//	}
-//	
-//	@Override
-//	protected ModelAndView onSubmit(HttpServletRequest request,
-//			HttpServletResponse response, Object command, BindException errors)
-//			throws Exception {
-//		CreateUserGroupCommand param = (CreateUserGroupCommand) command;
-//		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "name", "required");
-//		if (errors.hasErrors())
-//			return showForm(request, response, errors);
-//		userService.updateUserGroup(param.getGroupId(), param.getName(), param.getRole());
-//		return new ModelAndView("redirect:/user/showUserGroup?groupId=" + param.getGroupId());
-//	}
+	@Override
+	protected Object formBackingObject(HttpServletRequest request)
+			throws Exception {
+		if (isFormSubmission(request)) {
+			return super.formBackingObject(request);
+		}
+		CreateUserGroupCommand command = new CreateUserGroupCommand();
+		String groupId = request.getParameter("groupId");
+		UserGroup group = userService.getUserGroup(groupId);
+		if (group != null) {
+			command.setGroupName(group.getGroupName());
+			command.setRole(group.getRole());
+		}
+		return command;
+	}
+	
+	@Override
+	protected ModelAndView onSubmit(HttpServletRequest request,
+			HttpServletResponse response, Object command, BindException errors)
+			throws Exception {
+		CreateUserGroupCommand param = (CreateUserGroupCommand) command;
+		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "name", "required");
+		if (errors.hasErrors())
+			return showForm(request, response, errors);
+		userService.updateUserGroup(param.getGroupId(), param.getGroupName(), param.getRole());
+		return new ModelAndView("redirect:/user/list");
+	}
 }
