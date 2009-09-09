82114b721430aecafda6d9a99c03d9034d89dddb
shawn.won <shawn.won@18a45de0-8707-11de-82e5-a1837a67dc72>
2009-09-09 03:09:14 +0000

    테스트를 위한 기초 데이터 생성 SQL

--- org.jabberstory.cjac.consignsettle/sql/sample.sql
+++ org.jabberstory.cjac.consignsettle/sql/sample.sql
@@ -1,11 +1,11 @@
-insert into USERGROUP(GROUPID, NAME, ROLE) values ('admin', 'Administrators', 'A');
-insert into USERGROUP(GROUPID, NAME, ROLE) values ('accountant', 'Accountants', 'C');
+insert into USERGROUP(GROUPID, GROUPNAME, ROLE) values ('admin', 'Administrators', 'A');
+insert into USERGROUP(GROUPID, GROUPNAME, ROLE) values ('accountant', 'Accountants', 'C');
 
 insert into USER(USERID, USERNAME, EMAIL, PASSWORD) values ('admin', 'Administrator', 'admin@cjac.net', 'adminadmin');
 insert into USER(USERID, USERNAME, EMAIL, PASSWORD) values ('admin2', 'Administrator', 'admin2@cjac.net', 'adminadmin');
 insert into USER(USERID, USERNAME, EMAIL, PASSWORD) values ('shawn', '원상호', 'shawn.won@gmail.com', 'shawn');
 
-insert into FORUM(NAME) values ('public');
-insert into FORUM(NAME) values ('qna');
-insert into FORUM(NAME) values ('files');
+insert into FORUM(FORUMID, NAME) values (1, 'public');
+insert into FORUM(FORUMID, NAME) values (2, 'qna');
+insert into FORUM(FORUMID, NAME) values (3, 'files');
 
