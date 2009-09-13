8ddb58c9e3608d9c1a3bd32cb1ed407dfcd62d27
shawn.won <shawn.won@18a45de0-8707-11de-82e5-a1837a67dc72>
2009-09-13 08:45:32 +0000

    User객체를 ModelAndView에 항상 넣음.

--- org.jabberstory.cjac.consignsettle/src/org/jabberstory/cjac/consignsettle/common/util/CommonVariableInterceptorAdaptor.java
+++ org.jabberstory.cjac.consignsettle/src/org/jabberstory/cjac/consignsettle/common/util/CommonVariableInterceptorAdaptor.java
@@ -3,14 +3,27 @@ package org.jabberstory.cjac.consignsettle.common.util;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.jabberstory.cjac.consignsettle.domain.User;
+import org.jabberstory.cjac.consignsettle.domain.UserService;
+import org.springframework.security.Authentication;
+import org.springframework.security.context.SecurityContextHolder;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
 public class CommonVariableInterceptorAdaptor extends HandlerInterceptorAdapter {
+	private UserService userService;
+
+	public void setUserService(UserService userService) {
+		this.userService = userService;
+	}
+
 	@Override
 	public void postHandle(HttpServletRequest request,
 			HttpServletResponse response, Object handler,
 			ModelAndView mv) throws Exception {
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+		User user = userService.getUser(auth.getName());
+		mv.addObject("user", user);
 		mv.addObject("imgpath", request.getContextPath() + "/img");
 	}
 }
