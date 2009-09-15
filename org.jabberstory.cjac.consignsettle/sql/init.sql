b56f438466ff845232c3975a9ee8ff5d27197ebf
shawn.won <shawn.won@18a45de0-8707-11de-82e5-a1837a67dc72>
2009-09-15 11:15:53 +0000

    전담기관 생성할 때에, 그 기관을 위한 게시판 3개 생성함. 사용자별로 관련 전담기관의 게시판을 사용하게 됨.

--- org.jabberstory.cjac.consignsettle/sql/init.sql
+++ org.jabberstory.cjac.consignsettle/sql/init.sql
@@ -5,6 +5,6 @@ insert into USERGROUP(GROUPID, GROUPNAME, ROLE) values('2','회계법인','C');
 
 insert into USER(USERID, USERNAME, EMAIL, PASSWORD, GROUPID) values('admin','Administrator','admin@cjac.net','adminadmin', 1);
 
-insert into FORUM(FORUMID, NAME) values (1, '공지사항');
-insert into FORUM(FORUMID, NAME) values (2, 'Q &amp; A');
-insert into FORUM(FORUMID, NAME) values (3, '자료실');
+insert into FORUM(FORUMID, NAME, GROUPID, FORUMTYPE) values (1, '공지사항', "public", "public");
+insert into FORUM(FORUMID, NAME, GROUPID, FORUMTYPE) values (2, 'Q &amp; A', "public", "qna");
+insert into FORUM(FORUMID, NAME, GROUPID, FORUMTYPE) values (3, '자료실', "public", "files");
