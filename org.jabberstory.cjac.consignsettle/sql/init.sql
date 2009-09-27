5e22bd1ab8f0f8b0fe537fbeaf8f3b0bc0d9a212
greatshin2 <greatshin2@18a45de0-8707-11de-82e5-a1837a67dc72>
2009-09-27 08:59:19 +0000

    ENABLED 'Y' 추가

--- org.jabberstory.cjac.consignsettle/sql/init.sql
+++ org.jabberstory.cjac.consignsettle/sql/init.sql
@@ -5,6 +5,6 @@ insert into USERGROUP(GROUPID, GROUPNAME, ROLE) values('2','회계법인','C');
 
 insert into USER(USERID, USERNAME, EMAIL, PASSWORD, GROUPID) values('admin','Administrator','admin@cjac.net','adminadmin', 1);
 
-insert into FORUM(FORUMID, NAME, GROUPID, FORUMTYPE) values (1, '공지사항', "public", "public");
-insert into FORUM(FORUMID, NAME, GROUPID, FORUMTYPE) values (2, 'Q &amp; A', "public", "qna");
-insert into FORUM(FORUMID, NAME, GROUPID, FORUMTYPE) values (3, '자료실', "public", "files");
+insert into FORUM(FORUMID, NAME, GROUPID, FORUMTYPE, ENABLED) values (1, '공지사항', "public", "public", 'Y');
+insert into FORUM(FORUMID, NAME, GROUPID, FORUMTYPE, ENABLED) values (2, 'Q &amp; A', "public", "qna", "Y");
+insert into FORUM(FORUMID, NAME, GROUPID, FORUMTYPE, ENABLED) values (3, '자료실', "public", "files", 'Y');
